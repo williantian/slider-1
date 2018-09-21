2f8b1872ac3350d4fe67e59e3cdc1bb4a6e9dd1e
willian <williantian1995@gmail.com>
2018-09-21 18:13:47 +0800

    优化代码  删除注释

--- main.js
+++ main.js
@@ -1,28 +1,14 @@
 var n = 1
 初始化(n)
-
-//$('.images > img:nth-child(1)').addClass('current')
-//$('.images > img:nth-child(2)').addClass('enter')
-//$('.images > img:nth-child(3)').addClass('enter')
-
-
-
- 
- setInterval(function(){
-    //$(`.images > img:nth-child(${x(n)})`)
-    //.removeClass('current')
-    //.addClass('leave')
+setInterval(function(){
     makeLeave(getImage(n))
-    //.removeClass('current')
-    //.addClass('leave')
     .one('transitionend',function(e){
-        makeEnter($(e.currentTarget))//.removeClass('leave').addClass('enter')
+        makeEnter($(e.currentTarget))
     })
-    //$(`.images > img:nth-child(${x(n+1)})`)
     makeCurrent(getImage(n+1))
-    //.removeClass('enter').addClass('current')
     n = n + 1
  },3000)
+
 
 function x(n) {
     if(n>3){
